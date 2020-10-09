c93f7334e9e9ddc0484227b4435b9fbd38f3108d
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-08 16:53:59 -1000

    add project component + projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,10 +2,44 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
 
-const ProjectsPage = () => {
-  return <h2>projects page</h2>
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects-name">
+        <Projects projects={projects} title="all projects" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        description
+        github
+        id
+        title
+        url
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsPage
