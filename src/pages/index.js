8520ec6476c8f76dceac4f4382204037df83406d
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-08 16:31:05 -1000

    projects component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,42 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured" showLink />
     </Layout>
   )
 }
-// ...GatsbyImageSharpFluid
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        description
+        github
+        id
+        title
+        url
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
