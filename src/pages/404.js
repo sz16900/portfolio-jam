9ce8ebfa239d3908988bd4f3afc4fd8ab57ed449
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-09 12:48:16 -1000

    add about page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,14 @@ import { Link } from "gatsby"
 import SEO from "../components/SEO"
 
 const Error = () => {
-  return <h2>error page</h2>
+  return (
+    <Layout>
+      <main className="error-page">
+        <h1>Ooooops! It's a dead end</h1>
+        <Link className="btn" to="/">back home</Link>
+      </main>
+    </Layout>
+  )
 }
 
 export default Error
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,49 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
-}
 
+const About = ({data:{allStrapiAbout:{nodes}}}) => {
+  const {title, stack, image, info} = nodes[0];
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title}></Title>
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map((item)=>{
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+export const query = graphql`
+  {
+    allStrapiAbout {
+      nodes {
+        title
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+
+            }
+          }
+        }
+        info
+      }
+    }
+  }
+`
 export default About
